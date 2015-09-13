0d2899f3f1da4b283f2f868eff7df2598a80d685
Adam Armour <adam.armour@gmail.com>
2015-09-12 20:26:13 -0600

    feat: add DirectedGraph

---
+++ src/DirectedGraph.js
@@ -0,0 +1,43 @@
+export default class DirectedGraph {
+
+  constructor() {
+    this.vertices_ = new Map();
+  }
+
+  get edgeCount() {
+    // return [...this.vertices_.values()].reduce((count, set) => count += set.size, 0);
+
+    let count = 0;
+
+    for (let set of this.vertices_.values()) {
+      count += set.size;
+    }
+
+    return count;
+  }
+
+  get vertexCount() {
+    return this.vertices_.size;
+  }
+
+  addEdge(source, target) {
+    if (this.vertices_.has(source)) {
+      this.vertices_.get(source).add(target);
+    } else {
+      this.vertices_.set(source, new Set([target]));
+    }
+  }
+
+  getEdgesOf(vertex) {
+    let edges = new Set();
+
+    if (this.vertices_.has(vertex)) {
+      for (let target of this.vertices_.get(vertex)) {
+        edges.add({ source: vertex, target: target });
+      }
+    }
+
+    return edges;
+  }
+
+}
---
+++ src/DirectedGraph.spec.js
@@ -0,0 +1,67 @@
+import expect from 'expect.js';
+import DirectedGraph from './DirectedGraph';
+
+describe('new DirectedGraph', () => {
+
+  it('should have no vertices', () => {
+    expect((new DirectedGraph()).vertexCount).to.be(0);
+  });
+
+});
+
+describe('.addEdge(source, target)', () => {
+
+  it('should add the edge', () => {
+    let graph = new DirectedGraph();
+    graph.addEdge(1, 2);
+
+    expect(graph.edgeCount).to.be(1);
+  });
+
+  it('should add more than one edge with the same source vertex', () => {
+    let graph = new DirectedGraph();
+    graph.addEdge(1, 2);
+    graph.addEdge(1, 3);
+
+    expect(graph.edgeCount).to.be(2);
+  });
+
+  it('should not add the same edge more than once', () => {
+    let graph = new DirectedGraph();
+    graph.addEdge(1, 2);
+    graph.addEdge(1, 2);
+
+    expect(graph.edgeCount).to.be(1);
+  });
+
+});
+
+describe('.removeEdge(source, target)', () => {
+
+  it('should remove the edge');
+
+});
+
+describe('.getEdgesOf(vertex)', function () {
+
+  it('should return an empty collection if the vertex is not in the graph', () => {
+    let graph = new DirectedGraph();
+
+    expect(graph.getEdgesOf(1).size).to.eql(0);
+  });
+
+  it('should return an empty collection if there are no edges touching the specified vertex');
+
+  it('should return all of the edges touching the specified vertex', () => {
+    let graph = new DirectedGraph();
+    graph.addEdge(1, 2);
+    graph.addEdge(1, 3);
+
+    let edges = graph.getEdgesOf(1);
+
+    expect(edges.size).to.be(2);
+    expect(Array.from(edges)[0]).to.eql({ source: 1, target: 2 });
+    expect(Array.from(edges)[1]).to.eql({ source: 1, target: 3 });
+  });
+
+});
